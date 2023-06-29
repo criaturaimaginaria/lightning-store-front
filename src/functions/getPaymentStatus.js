d0cb85b60e055065b777cdb4f1775f1be64299df
criaturaimaginaria <80127608+criaturaimaginaria@users.noreply.github.com>
2023-06-29 13:08:44 -0300

    Update getPaymentStatus.js

--- src/functions/getPaymentStatus.js
+++ src/functions/getPaymentStatus.js
@@ -1,5 +1,6 @@
 export const getPaymentStatus = async (invoice) => {
-     const url = process.env.REACT_APP_API_URL_STATUS;
+    //  const url = process.env.REACT_APP_API_URL_STATUS;
+     const url = "https://lightning-store.onrender.com/status";
     const res = await fetch(url, {
       method: 'POST',
       headers: {
